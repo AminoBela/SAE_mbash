513d4f018b3c0508892e40e18d9ddfbd44822a36
ClemDW <clementdewasch54@gmail.com>
2025-01-14 14:12:04 +0100

    version1 liste

--- mbash.c
+++ mbash.c
@@ -10,16 +10,42 @@ char path[MAXLI];
 int pathidx;
 void mbash();
 
+char* historique[20];
+int size = 0;
+
+
+void history() {
+
+  for (int i = 0; i < size; i++) {
+    printf(": %s\n", historique[i]);
+  }
+
+}
+
+
 int main(int argc, char** argv) {
   while (1) {
     printf("Commande: ");
     fgets(cmd, MAXLI, stdin);
+    if (strcmp(cmd, "history") == 0) {
+      history();
+    }
     mbash(cmd);
   }
   return 0;
 }
 
 void mbash() {
+
+  // Ajout de la commande dans l'historique
+  if (size < 20) {
+
+    historique[size] = strdup(cmd);
+    size++;
+
+  }
+
   printf("Execute: %s", cmd);
   system(cmd);
+
 }
